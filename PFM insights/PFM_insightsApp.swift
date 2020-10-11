28dd7a88d2585d604944ae3fe5d1fdc1ce85cd6a
altria <junchen4933@gmail.com>
2020-10-11 21:33:43 +1300

    Initial Commit

---
+++ PFM insights/PFM_insightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PFM_insightsApp.swift
+//  PFM insights
+//
+//  Created by Altria on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct PFM_insightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
